076ccc119a993dd743cd0ed5fc89a69b415cf0e9
DavidZ <davidbill@outlook.com>
2020-07-24 01:57:21 +0800

    add: 404 page

--- src/js/pages/notfound.js
+++ src/js/pages/notfound.js
@@ -1,6 +1,9 @@
 const NOTFOUND_TEMPLATE = `
 <q-page padding>
-<div class="text-center">404 Not Found</div>
+  <router-link to="/annotation">
+    <q-img src="img/404.svg" style="max-width: 40%" alt="404" class="absolute-center">
+    </q-img>
+  </router-link>
 </q-page>
 `
 
