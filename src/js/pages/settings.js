d4b6c57385d1377518c69b828ef056c8dbecfbab
DavidZ <davidbill@outlook.com>
2020-07-24 20:24:57 +0800

    mod: add actionLabelData

--- src/js/pages/settings.js
+++ src/js/pages/settings.js
@@ -1,9 +1,10 @@
 const SETTINGS_TEMPLATE = `
-<q-page padding style="max-width: 800px; margin: 0 auto">
-<div class="text-h5">Settings</div>
-<div class="text-h6">Configuration</div>
-<label-table tableTitle="Object Label" dataName="objectLabelData"></label-table>
-<div class="text-h6">Preferences</div>
+<q-page padding style="max-width: 800px; margin: 0 auto" class="q-gutter-md">
+  <div class="text-h5">Settings</div>
+  <div class="text-h6">Configuration</div>
+  <label-table tableTitle="Object Labels" dataName="objectLabelData"></label-table>
+  <label-table tableTitle="Action Labels" dataName="actionLabelData"></label-table>
+  <div class="text-h6">Preferences</div>
 </q-page>
 `
 
--- src/js/store/modules/settings.js
+++ src/js/store/modules/settings.js
@@ -23,12 +23,44 @@ export default {
           color: '#ff00ff',
         },
       ],
+    actionLabelData: JSON.parse(localStorage.getItem('actionLabelData')) ||
+      [
+        {
+          id: 0,
+          name: '<none>',
+          color: '#00ff00',
+        },
+        {
+          id: 1,
+          name: 'walk',
+          color: '#ff0000',
+        },
+        {
+          id: 2,
+          name: 'run',
+          color: '#0000ff',
+        },
+        {
+          id: 3,
+          name: 'swim',
+          color: '#ff00ff',
+        },
+        {
+          id: 4,
+          name: 'fly',
+          color: '#00ffff',
+        },
+      ],
   }),
   getters: {},
   mutations: {
     objectLabelData (state, value) {
       state.objectLabelData = value
       localStorage.setItem('objectLabelData', JSON.stringify(value))
+    },
+    actionLabelData (state, value) {
+      state.actionLabelData = value
+      localStorage.setItem('actionLabelData', JSON.stringify(value))
     },
   },
   actions: {},
