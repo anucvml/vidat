754993118bc590092788faf645cbc7e1f9bec235
DavidZ <davidbill@outlook.com>
2020-08-12 20:00:16 +0800

    :lipstick: related to #36, help

--- src/js/pages/help/help.js
+++ src/js/pages/help/help.js
@@ -3,28 +3,77 @@ const HELP_TEMPLATE = `
   <div class="text-h5">Help (TODO)</div>
   <div class="text-body1">Click the top-left menu button (☰) to load and save data.</div>
   <div class="text-body1">Click and drag on frame to add a new bounding box.</div>
-  <div class="text-h5">Keyboard Shortcuts (TODO)</div>
-  <pre>    h           :: help (this window)
-    p           :: play video segment
-    comma, <    :: advance to next keyframe
-    period, >   :: retreat to previous keyframe
-    [, {        :: select left slider
-    ], }        :: select right slider
-    left-arrow  :: previous frame (when slider is selected)
-    right-arrow :: next frame (when slider is selected)
-    page-up     :: jump to previous 10% of video (when slider is selected)
-    page-down   :: jump to next 10% of video (when slider is selected)
-    plus (+)    :: add a new segment
-    delete      :: delete currently active object
-    shift       :: duplicate currently active object when mouse down
-    tab         :: move to next field when editing objects</pre>
+  <div class="text-h5">Keyboard Shortcuts</div>
+  <div class="row q-ma-none" v-for="shortcut in shortcuts" :key="shortcut.key">
+    <div class="col text-center">
+      <q-chip size='md'>{{ shortcut.key }}</q-chip>
+    </div>
+    <div class="col text-body1" style="line-height: 30px;">
+      {{ shortcut.description }}
+    </div>
+  </div>
   <div class="text-h5">Tutorial Videos (TODO)</div>
 </div>
 `
 
 export default {
   data: () => {
-    return {}
+    return {
+      shortcuts: [
+        {
+          key: 'p',
+          description: 'play video segment',
+        },
+        {
+          key: 'comma, <',
+          description: 'advance to next keyframe',
+        },
+        {
+          key: 'period, >',
+          description: 'retreat to previous keyframe',
+        },
+        {
+          key: '[, {',
+          description: 'select left slider(TODO)',
+        },
+        {
+          key: '], }',
+          description: 'select right slider(TODO)',
+        },
+        {
+          key: 'left-arrow',
+          description: 'previous frame (when slider is selected)',
+        },
+        {
+          key: 'right-arrow',
+          description: 'next frame (when slider is selected)',
+        },
+        {
+          key: 'page-up',
+          description: 'jump to previous 10% of video (when slider is selected)',
+        },
+        {
+          key: 'page-down',
+          description: 'jump to next 10% of video (when slider is selected)',
+        },
+        {
+          key: 'plus (+)',
+          description: 'add a new action / video segment',
+        },
+        {
+          key: 'delete',
+          description: 'delete currently active object',
+        },
+        {
+          key: 'shift',
+          description: 'duplicate currently active object when mouse down',
+        },
+        {
+          key: 'tab',
+          description: 'move to next field when editing objects',
+        },
+      ],
+    }
   },
   methods: {},
   template: HELP_TEMPLATE,
