72cc3b19f85116bb3f1efe2a5e33ec751897964b
DavidZ <davidbill@outlook.com>
2020-09-07 15:15:26 +0800

    :lipstick: make skeleton page correspond to layout

--- src/js/pages/annotation/components/annotationSkeleton.js
+++ src/js/pages/annotation/components/annotationSkeleton.js
@@ -1,5 +1,6 @@
 const ANNOTATION_SKELETON_TEMPLATE = `
 <div>
+  <div class="row" style="min-height: 100px"><q-skeleton class="col q-ma-sm"></q-skeleton></div>
   <div class="row" style="min-height: 400px">
     <q-skeleton class="col q-ma-sm"></q-skeleton>
     <q-skeleton class="col-1 q-ma-sm"></q-skeleton>
@@ -10,7 +11,6 @@ const ANNOTATION_SKELETON_TEMPLATE = `
     <div class="col-1 q-ma-sm"></div>
     <q-skeleton class="col q-ma-sm"></q-skeleton>
   </div>
-  <div class="row" style="min-height: 100px"><q-skeleton class="col q-ma-sm"></q-skeleton></div>
   <div class="row" style="min-height: 100px"><q-skeleton class="col q-ma-sm"></q-skeleton></div>
 </div>
 `
