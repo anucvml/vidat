f07d66022f1c6976fc53e7bf4b3f977a3411b48e
Jiahao Zhang <jiahao.zhang@anu.edu.au>
2023-03-03 16:30:59 +1100

    :bug: fix #175

--- src/worker/video-process-worker.js
+++ src/worker/video-process-worker.js
@@ -1,72 +1,28 @@
 // Modified based on https://github.com/w3c/webcodecs/tree/main/samples/mp4-decode
 import MP4Box from 'mp4box'
 
-class Writer {
-  constructor (size) {
-    this.data = new Uint8Array(size)
-    this.idx = 0
-    this.size = size
-  }
-
-  getData () {
-    if (this.idx !== this.size)
-      throw 'Mismatch between size reserved and sized used'
-
-    return this.data.slice(0, this.idx)
-  }
-
-  writeUint8 (value) {
-    this.data.set([value], this.idx)
-    this.idx++
-  }
-
-  writeUint16 (value) {
-    const arr = new Uint16Array(1)
-    arr[0] = value
-    const buffer = new Uint8Array(arr.buffer)
-    this.data.set([buffer[1], buffer[0]], this.idx)
-    this.idx += 2
-  }
-
-  writeUint8Array (value) {
-    this.data.set(value, this.idx)
-    this.idx += value.length
-  }
-}
-
 const getConfig = (info, mp4File) => {
   const track = info.videoTracks[0]
-  const avccBox = mp4File.moov.traks[0].mdia.minf.stbl.stsd.entries[0].avcC
-  let i
-  let size = 7
-  for (i = 0; i < avccBox.SPS.length; i++) {
-    size += 2 + avccBox.SPS[i].length
+  // get description, adapted from
+  // https://github.com/w3c/webcodecs/blob/main/samples/video-decode-display/demuxer_mp4.js#L64
+  let description = undefined
+  for (const entry of mp4File.moov.traks[0].mdia.minf.stbl.stsd.entries) {
+    if (entry.avcC || entry.hvcC) {
+      const stream = new MP4Box.DataStream(undefined, 0,
+        MP4Box.DataStream.BIG_ENDIAN)
+      if (entry.avcC) {
+        entry.avcC.write(stream)
+      } else {
+        entry.hvcC.write(stream)
+      }
+      description = new Uint8Array(stream.buffer, 8)  // Remove the box header.
+    }
   }
-  for (i = 0; i < avccBox.PPS.length; i++) {
-    size += 2 + avccBox.PPS[i].length
-  }
-  const writer = new Writer(size)
-  writer.writeUint8(avccBox.configurationVersion)
-  writer.writeUint8(avccBox.AVCProfileIndication)
-  writer.writeUint8(avccBox.profile_compatibility)
-  writer.writeUint8(avccBox.AVCLevelIndication)
-  writer.writeUint8(avccBox.lengthSizeMinusOne + (63 << 2))
-  writer.writeUint8(avccBox.nb_SPS_nalus + (7 << 5))
-  for (i = 0; i < avccBox.SPS.length; i++) {
-    writer.writeUint16(avccBox.SPS[i].length)
-    writer.writeUint8Array(avccBox.SPS[i].nalu)
-  }
-  writer.writeUint8(avccBox.nb_PPS_nalus)
-  for (i = 0; i < avccBox.PPS.length; i++) {
-    writer.writeUint16(avccBox.PPS[i].length)
-    writer.writeUint8Array(avccBox.PPS[i].nalu)
-  }
-
   return {
     codec: track.codec,
     codedHeight: track.track_height,
     codedWidth: track.track_width,
-    description: writer.getData()
+    description: description,
   }
 }
 
@@ -83,13 +39,22 @@ onmessage = async (event) => {
     mp4File.onReady = info => {
       isMoovFound = true
       const videoTrack = info.tracks.find(track => track.type === 'video')
-      const { id, track_width, track_height, nb_samples, movie_duration, movie_timescale } = videoTrack
+      const {
+        id,
+        track_width,
+        track_height,
+        nb_samples,
+        movie_duration,
+        movie_timescale,
+      } = videoTrack
       offscreen.width = track_width
       offscreen.height = track_height
       const duration = movie_duration / movie_timescale
       const probeFps = nb_samples / duration
       const probeFrames = nb_samples
-      const fps = probeFps < event.data.defaultFps ? probeFps : event.data.defaultFps
+      const fps = probeFps < event.data.defaultFps
+        ? probeFps
+        : event.data.defaultFps
       const frames = Math.floor(fps * duration)
       postMessage({
         videoTrackInfo: {
@@ -97,8 +62,8 @@ onmessage = async (event) => {
           height: track_height,
           duration: duration,
           frames,
-          fps
-        }
+          fps,
+        },
       })
       const frameIndexList = []
       for (let i = 0; i < frames; i++) {
@@ -111,16 +76,20 @@ onmessage = async (event) => {
           if (frameIndexList.includes(_currentFrameIndex)) {
             ctx.drawImage(frame, 0, 0)
             offscreen.convertToBlob({ type: 'image/jpeg' }).then(blob => {
-              postMessage({ frame: blob, frameIndex: frameIndexList.indexOf(_currentFrameIndex) })
+              postMessage({
+                frame: blob,
+                frameIndex: frameIndexList.indexOf(_currentFrameIndex),
+              })
             })
-            if (frameIndexList.indexOf(_currentFrameIndex) >= frameIndexList.length - 3) { // TODO: hack, safe margin
+            if (frameIndexList.indexOf(_currentFrameIndex) >=
+              frameIndexList.length - 3) { // TODO: hack, safe margin
               postMessage({ done: true })
             }
           }
           frame.close()
           currentFrameIndex += 1
         },
-        error: error => console.error('VideoDecoder: ', error)
+        error: error => console.error('VideoDecoder: ', error),
       })
       decoder.configure(getConfig(info, mp4File))
       mp4File.setExtractionOptions(id)
@@ -136,7 +105,7 @@ onmessage = async (event) => {
           type: type,
           timestamp: sample.cts,
           duration: sample.duration,
-          data: sample.data
+          data: sample.data,
         })
         decoder.decode(chunk)
       }
@@ -151,13 +120,22 @@ onmessage = async (event) => {
         if (isLeft) {
           headers['Range'] = `bytes=${leftOffset}-`
         } else {
-          headers['Range'] = `bytes=${rightOffset}-${rightOffset + probeUnit - 1}`
+          headers['Range'] = `bytes=${rightOffset}-${rightOffset + probeUnit -
+          1}`
         }
-        const response = await fetch((new URL(event.data.src, new URL('..', event.target.location)).href), {
-          signal: abortController.signal,
-          headers
-        })
-        if (!response.ok) throw { type: 'fetch', status: response.status, statusText: response.statusText }
+        const response = await fetch(
+          (new URL(event.data.src, new URL('..', event.target.location)).href),
+          {
+            signal: abortController.signal,
+            headers,
+          })
+        if (!response.ok) {
+          throw {
+            type: 'fetch',
+            status: response.status,
+            statusText: response.statusText,
+          }
+        }
         if (isLeft && contentLength === 0) {
           contentLength = response.headers.get('Content-Length')
           rightOffset = contentLength - probeUnit
@@ -174,11 +152,7 @@ onmessage = async (event) => {
             } else if (isLeft && !isMoovFound) {
               console.error('Error')
               return false
-            } else if (!isLeft && isMoovFound) {
-              return true
-            } else {
-              return false
-            }
+            } else return !isLeft && isMoovFound;
           }
           const buffer = value.buffer
           buffer.fileStart = isLeft ? leftOffset : offset
@@ -190,7 +164,8 @@ onmessage = async (event) => {
           }
         }
       } catch (e) {
-        if (e.toString() === 'AbortError: The user aborted a request.') return false
+        if (e.toString() ===
+          'AbortError: The user aborted a request.') return false
         else throw e
       }
     }
