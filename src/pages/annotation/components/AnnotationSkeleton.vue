566c424f37aaa425575584f71b05d681ee56f7d7
DavidZ <davidbill@outlook.com>
2023-11-15 11:25:50 +1100

    💄 center logo

--- src/pages/annotation/components/AnnotationSkeleton.vue
+++ src/pages/annotation/components/AnnotationSkeleton.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="column flex-center">
+  <div class="absolute fit column items-center justify-center">
     <img
         v-if="$q.dark.isActive"
-        style="width: 150px; padding: 100px 0 50px 0;"
+        style="width: 150px; padding-bottom: 50px;"
         src="img/logo-dark.svg"
         alt="Logo"
     >
     <img
         v-else
-        style="width: 150px; padding: 100px 0 50px 0;"
+        style="width: 150px; padding-bottom: 50px;"
         src="img/logo.svg"
         alt="Logo"
     >
     <div class="text-h4">Vidat</div>
     <VersionBadge/>
     <q-btn-group
-        style="margin-top: 30px;"
+        style="padding-top: 30px;"
         flat
     >
       <q-btn
@@ -33,26 +33,26 @@
           @click="handleLoad"
       />
     </q-btn-group>
-    <div style="margin-top: 20px;">
+    <div style="padding: 20px 0;">
       <a
       class="text-body1"
-          style="margin: 0 10px; text-decoration: underline;"
+          style="padding: 0 10px; text-decoration: underline;"
           href="/?annotation=/annotation/example.json"
       >Demo</a>
       <router-link
           class="text-body1"
-          style="margin: 0 10px; text-decoration: underline;"
+          style="padding: 0 10px; text-decoration: underline;"
           to="/help"
       >Help</router-link>
       <a
           class="text-body1"
-          style="margin: 0 10px; text-decoration: underline;"
+          style="padding: 0 10px; text-decoration: underline;"
           target="_blank"
           href="https://www.youtube.com/watch?v=XsxGac7Z4Wk&list=PLD-7XrNHCcFLv938DO4yYcTrgaff9BJjN"
       >Tutorial Videos</a>
       <router-link
           class="text-body1"
-          style="margin: 0 10px; text-decoration: underline;"
+          style="padding: 0 10px; text-decoration: underline;"
           to="/about"
       >About</router-link>
     </div>
